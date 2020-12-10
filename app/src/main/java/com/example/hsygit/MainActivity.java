b7a59179e0d3fc7b8c0ddf08f24a3c7e15b231d6
hansiyuyu <2051303328@qq.com>
2020-12-10 18:32:45 +0800

    第三次提交

--- app/src/main/java/com/example/hsygit/MainActivity.java
+++ app/src/main/java/com/example/hsygit/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String A= "第一次提交";
+        String A = "第一次提交";
+        String B = "123";
     }
 }
